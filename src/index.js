597ab3f055c9360e964d0595c0e3e232e9e1412e
SalamatovAhmed <_email@example.com>
2026-02-28 15:55:31 +0600

    Fix: chakra provider and router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react'; // Добавили импорт theme
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <ChakraProvider>
+    <ChakraProvider theme={theme}> {/* Передали стандартную тему */}
+      <HashRouter>
         <App />
-      </ChakraProvider>
-    </HashRouter>
+      </HashRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
